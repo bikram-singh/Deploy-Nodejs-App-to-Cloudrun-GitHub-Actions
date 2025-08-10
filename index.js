8bb4ad9de1ccfc3221b868fb4205e30278719683
bikramgcpcloud1 <bikramgcpcloud1@gmail.com>
2025-08-10 11:13:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.get('/work', (req, res) => {
+  res.send('work');
+});
+
+app.listen(port, () => {
+  console.log(`Node.js app listening on port ${port}`);
+  console.log(`Access it locally at http://localhost:${port}`);
+});
